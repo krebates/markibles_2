6486d25eb1dab02b5dca9d8d953d40399e7b2294
Krystle Bates <krebates@gmail.com>
2014-01-12 19:00:06 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Markibles2::Application.config.secret_key_base = '9e3c2ab5872b9afd41dc41e78046c829a8d82c106ec2ff31da0e98ec3bbf127f53f3e157248042f4aa85162e263451c6be76b8a50111ce48a11ac01de04b04a9'
