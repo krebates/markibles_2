a1b10755f945c3bef14829d794aa701787b2f9f9
Krystle Bates <krebates@gmail.com>
2014-01-13 19:51:37 -0500

    resized pictures

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,7 +25,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_fit  => [200, 300]
   #
   # def scale(width, height)
   #   # do something
