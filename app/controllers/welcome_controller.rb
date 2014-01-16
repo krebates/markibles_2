adaa9858586c168e5a0aa239e25063fc8dc351ae
Krystle Bates <krebates@gmail.com>
2014-01-16 15:03:50 -0500

    updated file to have masonry

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 
   def index
-    @product_pics = Product.all.map{|product| product.image}.compact
+    @product_pics = Product.all.keep_if{|product| product.image != nil}
   end
 end
